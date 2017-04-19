648fc03786960d94849eef856a43f08331b68350
Valentina Endovitskaya <valyalark@hotmail.com>
2017-04-19 18:41:17 +0300

    fixed analog w

--- WatchPatterns/Decorator/AnalogTimeDecorator.cs
+++ WatchPatterns/Decorator/AnalogTimeDecorator.cs
@@ -15,12 +15,12 @@ namespace WatchPatterns
             Bitmap bitmap = new Bitmap(150, 150);
             Graphics grBuffer = Graphics.FromImage(bitmap);
             grBuffer.DrawImage(Image.FromFile("clock.jpg"), 0, 0);
-            grBuffer.DrawLine(new Pen(Brushes.Black, 1), 75, 75, (float)(65 * Math.Sin(this.Time.Seconds * 6 * (Math.PI / 180)) + 75), (float)(55 * Math.Cos(this.Time.Seconds * 6 * (Math.PI / 180)) * (-1) + 75));
-            grBuffer.DrawLine(new Pen(Brushes.Black, 2), 75, 75, (float)(60 * Math.Sin(this.Time.Minutes*6* (Math.PI / 180)) + 75), (float)(55 * Math.Cos(this.Time.Minutes * 6 * (Math.PI / 180)) * (-1) + 75));
+            grBuffer.DrawLine(new Pen(Brushes.Black, 1), 75, 75, (float)(65 * Math.Sin(this.Time.Seconds * 6 * (Math.PI / 180)) + 75), (float)(65 * Math.Cos(this.Time.Seconds * 6 * (Math.PI / 180)) * (-1) + 75));
+            grBuffer.DrawLine(new Pen(Brushes.Black, 2), 75, 75, (float)(60 * Math.Sin(this.Time.Minutes*6* (Math.PI / 180)) + 75), (float)(60 * Math.Cos(this.Time.Minutes * 6 * (Math.PI / 180)) * (-1) + 75));
             double h = (this.Time.Hours);
             if (h > 12)  h -= 12;
             h = (h+ this.Time.Minutes / 60.0)*30*(Math.PI / 180);
-            grBuffer.DrawLine(new Pen(Brushes.Black, 3), 75, 75, (float)( 55*Math.Sin(h) +75), (float)(55* Math.Cos(h)*(-1)  + 75));
+            grBuffer.DrawLine(new Pen(Brushes.Black, 3), 75, 75, (float)( 50*Math.Sin(h) +75), (float)(50* Math.Cos(h)*(-1)  + 75));
             graphTime.DrawImage(bitmap, 0, 0);
             grBuffer.Dispose();
             base.Draw(lb, graphTime);
